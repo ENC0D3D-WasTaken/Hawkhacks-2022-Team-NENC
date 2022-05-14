139eb65c05b98ce0f741c5835bb87f991acdf7aa
DrissyChan <chrissapostan2@gmail.com>
2022-05-14 16:21:42 +0300

    database tables inclusion

---
+++ database.py
@@ -0,0 +1,90 @@
+from msilib.schema import Class
+from unicodedata import name
+from sqlalchemy import ARRAY, Boolean, Column, DateTime, ForeignKey,String ,Integer, create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy_utils import database_exists, create_database
+from settings import settings
+
+def engine_init(settings):
+    url = f'postgresql+psycopg2://{settings["username"]}:{settings["password"]}@{settings["host"]}:{settings["port"]}/{settings["db"]}'
+    if not database_exists(url):
+        create_database(url)
+    engine = create_engine(url,pool_size=50,echo=False)
+    return engine
+
+engine = engine_init(settings)
+
+session = sessionmaker(autocommit=False, autoflush=False, bind=engine)()
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'userlist'
+    id = Column(Integer, primary_key=True)
+    memberId = Column(Integer,unique=True)
+    name = Column(String(),unique=True)
+    timestamp = Column(DateTime)
+
+class NFTCollections(Base):
+    __tablename__ = 'nftcollections'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(),unique=True)
+    description = Column(String())
+    mintPrice = Column(Integer())
+    timestamp = Column(DateTime())
+
+class NFTCollectionItems(Base):
+    __tablename__ = 'nftcollectionitems'
+    id = Column(Integer, primary_key=True)
+    collectionId = Column(Integer, ForeignKey('nftcollections.id'))
+    name = Column(String(),unique=True)
+    timestamp = Column(DateTime())
+
+class CryptoCurrencies(Base):
+    __tablename__ = 'cryptocurrencylist'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(),unique=True)
+    price = Column(Integer())
+    quantity = Column(Integer())
+    timestamp = Column(DateTime())
+    
+class CryptoCurrencyTransactions(Base):
+    __tablename__ = 'cryptocurrencytransactions'
+    id = Column(Integer, primary_key=True)
+    userId = Column(Integer, ForeignKey('userlist.id'))
+    cryptoId = Column(Integer, ForeignKey('cryptocurrencylist.id'))
+    quantity = Column(Integer())
+    boughtFor = Column(Integer())
+    timestamp = Column(DateTime())
+
+class NFTMint(Base):
+    __tablename__ = 'nfttransactions'
+    id = Column(Integer, primary_key=True)
+    boughtFor = Column(Integer())
+    timestamp = Column(DateTime())
+
+class CryptoforNFTTrade(Base):
+    __tablename__ = 'cryptofornfttrade'
+    id = Column(Integer, primary_key=True)
+    buyer = Column(String, ForeignKey('userlist.name'))
+    seller = Column(String, ForeignKey('userlist.name'))
+    CryptoAmount = Column(Integer())
+    CryptoType = Column(String, ForeignKey('cryptocurrencylist.name'))
+    NFTType = Column(String, ForeignKey('nftcollections.name'))
+    NFTname = Column(String(),ForeignKey('nftcollectionitems.name'))
+    timestamp = Column(DateTime())
+
+class NFTforCryptoTrade(Base):
+    __tablename__ = 'nftforcryptotrade'
+    id = Column(Integer, primary_key=True)
+    buyer = Column(String, ForeignKey('userlist.name'))
+    seller = Column(String, ForeignKey('userlist.name'))
+    CryptoAmount = Column(Integer())
+    CryptoType = Column(String, ForeignKey('cryptocurrencylist.name'))
+    NFTType = Column(String, ForeignKey('nftcollections.name'))
+    NFTname = Column(String,ForeignKey('nftcollectionitems.name'))
+    timestamp = Column(DateTime())
+    
+
+Base.metadata.create_all(bind=engine)
--- main.py
+++ main.py
@@ -3,18 +3,29 @@ from discord.ext import commands
 from discord.utils import get
 from os import getenv
 from dotenv import load_dotenv
+import database
+from database import session as db
 
 load_dotenv()
 
 intents = discord.Intents().all()
 activity = discord.Activity(type=discord.ActivityType.watching, name="Anything.")
 
-bot = commands.Bot(command_prefix='!',intents=intents, activity=activity, status=discord.Status.online)
+bot = commands.Bot(command_prefix='$',intents=intents, activity=activity, status=discord.Status.online)
 
 @bot.event
 async def on_ready():
     print('Bot is ready.')
-    
+
+@bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.CommandNotFound):
+        await ctx.send('Command not found.')
+
+@bot.command(aliases=['JOIN, j, J'])
+async def join(ctx):
+    return
+
 if __name__ == '__main__':
     TOKEN = getenv('TOKEN')
     bot.run(TOKEN)
